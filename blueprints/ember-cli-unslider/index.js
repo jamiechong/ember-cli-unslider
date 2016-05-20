2cd39c26185c75bc612003e8da4db6a225dc3989
Jamie Chong <jamie@hippocurious.com>
2016-05-20 00:13:43 -0700

    sort out bower dependencies for addon

--- blueprints/ember-cli-unslider/index.js
+++ blueprints/ember-cli-unslider/index.js
@@ -1,9 +1,9 @@
 /*jshint node:true*/
 module.exports = {
-  description: ''
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+  description: '',
 
   afterInstall: function(options) {
-
     return this.addBowerPackagesToProject([
       { name: 'jquery.event.move', target: '^1.3.6' }, 
       { name: 'unslider',          source: 'https://github.com/idiot/unslider.git#master' }
