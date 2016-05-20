d6c35579ca14d44e44795fd71d3e99f401e34d6a
Jamie Chong <jamie@hippocurious.com>
2016-05-20 00:03:37 -0700

    try to fix blueprint using github bower package

--- blueprints/ember-cli-unslider/index.js
+++ blueprints/ember-cli-unslider/index.js
@@ -6,7 +6,7 @@ module.exports = {
 
     return this.addBowerPackagesToProject([
       { name: 'jquery.event.move', target: '^1.3.6' }, 
-      { name: 'unslider',          source: 'https://github.com/idiot/unslider.git', target: 'master' }
+      { name: 'unslider',          source: 'https://github.com/idiot/unslider.git#master' }
     ]);    
   }
 };
